8733c4b5bd7c3967c7a8e36e4c049f215a61d405
devfabioduarte <fabioapdu2024@gmail.com>
2025-06-20 14:03:47 -0300

    Remove .env from version control

--- src/pages/Characters/index.tsx
+++ src/pages/Characters/index.tsx
@@ -8,6 +8,7 @@ import { CharactersGrid } from "../../components/Card/styles";
 import { Pagination } from "../../components/Pagination";
 import { useSearchParams } from "react-router";
 import { Loading } from "../../components/Loading";
+import { CharacterDetail } from "../../components/CharacterInfo";
 
 export function Characters() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -15,6 +16,7 @@ export function Characters() {
   const [totalPages, setTotalPages] = useState(0);
   const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [selectedId, setSelectedId] = useState<number | null>(null);
 
   const currentPage = Number(searchParams.get("page")) || 1;
   const currentSearch = searchParams.get("search") || "";
@@ -58,9 +60,17 @@ export function Characters() {
         <>
           <CharactersGrid>
             {characters.map((character) => (
-              <Card key={character.id} character={character} />
+              <Card
+                key={character.id}
+                character={character}
+                onClick={() => setSelectedId(character.id)}
+              />
             ))}
           </CharactersGrid>
+
+          {/* MODAL */}
+          {selectedId !== null && <CharacterDetail id={selectedId} />}
+
           <Pagination
             currentPage={currentPage}
             totalPages={totalPages}
